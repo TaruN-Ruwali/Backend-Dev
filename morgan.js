4f980e2d39b1b864ddc9a090cd15b5afadd5e44b
ROGER <tarunruwali1610@gmail.com>
2025-08-19 21:52:45 +0530

    Modified

--- morgan.js
+++ morgan.js
@@ -4,9 +4,24 @@ const app = express();
 
 app.use(morgan('dev'));
 
+// app.use((req, res, next) => {    //     // This is a middleware function that runs for every request
+//     console.log('This is a middleware function');
+//     next();
+// });
+
 app.set('view engine', 'ejs');
 
-app.get('/',(req,res) => {
+app.get('/',
+    (req,res,next) => {  //Creating a middleware function like this run it for / route only
+
+    const a = 10;
+    const b = 20;
+    console.log(a + b);
+    next();     //Very neccessary to call next() to pass control to the next middleware or route handler without which the request will hang indefinitely.
+
+} ,
+    (req,res) => {
+        
     res.render("index");
 })
 
